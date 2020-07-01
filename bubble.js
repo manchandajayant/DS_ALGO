1d7d1cbc591638936cc08981cf5b91c853c9b3e3
manchandajayant <maanjayant@gmail.com>
2020-07-01 16:42:35 +0200

    first

---
+++ bubble.js
@@ -0,0 +1,25 @@
+console.log("bubble");
+
+//bubble sort
+//bubble sort works on comparing and swapping elements
+//the largest value bubbles to the top
+
+//less optimised way
+function bubbleSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      //swap
+      //console.log(arr);
+      if (arr[j] > arr[j + 1]) {
+        console.log("arr[j]", arr[j], arr[j + 1]);
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        console.log("arr[j]=arr[j+1]", arr[j + 1], temp);
+        arr[j + 1] = temp;
+        console.log("temp", temp, arr[j]);
+      }
+    }
+  }
+  return arr;
+}
+bubbleSort([13, 4, 29, 90, 1, 3, 10, 0]);
