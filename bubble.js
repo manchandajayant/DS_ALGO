51067f8930472686fca7d24019ce07f691b28fb3
manchandajayant <maanjayant@gmail.com>
2020-07-01 16:50:18 +0200

    first

--- bubble.js
+++ bubble.js
@@ -11,15 +11,36 @@ function bubbleSort(arr) {
       //swap
       //console.log(arr);
       if (arr[j] > arr[j + 1]) {
-        console.log("arr[j]", arr[j], arr[j + 1]);
+        //console.log("arr[j]", arr[j], arr[j + 1]);
         var temp = arr[j];
         arr[j] = arr[j + 1];
-        console.log("arr[j]=arr[j+1]", arr[j + 1], temp);
+        //console.log("arr[j]=arr[j+1]", arr[j + 1], temp);
         arr[j + 1] = temp;
-        console.log("temp", temp, arr[j]);
+        //console.log("temp", temp, arr[j]);
       }
     }
   }
   return arr;
 }
-bubbleSort([13, 4, 29, 90, 1, 3, 10, 0]);
+console.log(bubbleSort([13, 4, 29, 90, 1, 3, 10, 0]));
+
+//optimised way
+function bubbleSortOptimised(arr) {
+  for (var i = arr.length; i > 0; i--) {
+    for (var j = 0; j < i - 1; j++) {
+      //swap
+      //console.log(arr);
+      if (arr[j] > arr[j + 1]) {
+        //console.log("arr[j]", arr[j], arr[j + 1]);
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        //console.log("arr[j]=arr[j+1]", arr[j + 1], temp);
+        arr[j + 1] = temp;
+        //console.log("temp", temp, arr[j]);
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSortOptimised([13, 4, 29, 90, 1, 3, 10, 0]));
